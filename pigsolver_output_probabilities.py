ba92bc56ac086f89f57ca877658e2a2457c43669
Shmuli Feld <shmulifeld@Shmulis-MacBook-Air.local>
2025-04-18 00:42:52 -0400

    probabilities recorded

---
+++ pigsolver_output_probabilities.py
@@ -0,0 +1,65 @@
+import csv
+
+GOAL = 100
+EPSILON = 1e-9
+
+p = [[[0 for k in range(GOAL)] for j in range(GOAL)] for i in range(GOAL)]
+flip = [[[False for k in range(GOAL)] for j in range(GOAL)] for i in range(GOAL)]
+
+def valueIterate():
+    maxChange = EPSILON # Starting here to make sure we enter loop
+    while maxChange >= EPSILON:
+        maxChange = 0.0
+        for i in range(GOAL): # i = PLAYER SCORE
+            for j in range(GOAL): # j = OPPONENT SCORE
+                for k in range(GOAL-i): # k = TURN TOTAL SO FAR
+                    oldProb = p[i][j][k]
+                    pFlip = (
+                        (1/6) * ( 1.0
+                            - pWin(j, i, 0) 
+                            + pWin(i, j, k + 2)
+                            + pWin(i, j, k + 3)
+                            + pWin(i, j, k + 4)
+                            + pWin(i, j, k + 5)
+                            + pWin(i, j, k + 6)
+                        )
+                    )
+                    pHold = 1.0 - pWin(j, i+k, 0)
+                    p[i][j][k] = max(pFlip, pHold)
+                    flip[i][j][k] = pFlip > pHold
+                    change = abs(p[i][j][k] - oldProb)
+                    maxChange = max(maxChange, change)
+
+def pWin(i, j, k):
+    if i+k >= GOAL:
+        return 1.0
+    elif j >= GOAL:
+        return 0.0
+    else:
+        return p[i][j][k]
+
+def outputHoldValues():
+    for i in range(GOAL):
+        for j in range(GOAL):
+            k = 0
+            while k < GOAL - i and flip[i][j][k]:
+                k += 1
+            print(k,end=' ')
+        print()
+
+def writePToCSV(filename="probabilities.csv"):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["i", "j", "k", "p_value"])  # header
+        for i in range(GOAL):
+            for j in range(GOAL):
+                for k in range(GOAL - i):
+                    writer.writerow([i, j, k, p[i][j][k]])
+
+def main():
+    valueIterate()
+    # outputHoldValues()
+    writePToCSV()
+
+if __name__ == '__main__':
+    main()
