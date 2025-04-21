5ef9f74aa34e9550c95d7621f549d15e67a10b52
Shmuli Feld <shmulifeld@Shmulis-Air.quinnipiac.edu>
2025-04-21 11:54:49 -0400

    fixed visualizer

--- results/piglet_strategy_visualizer.py
+++ results/piglet_strategy_visualizer.py
@@ -1,8 +1,13 @@
 import plotly.graph_objects as go
 import pandas as pd
+import os
+
+# Get the absolute path of the directory where this script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "hold_policy.csv")
 
 # Load CSV and clean up extra columns
-df = pd.read_csv("hold_policy.csv", index_col=0)
+df = pd.read_csv(file_path, index_col=0)
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')]  # Drop bad columns
 
 # Convert axis labels to int
